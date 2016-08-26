7ee39745d9b0d09090c7458fbb2aba3a86c59dc4
Ryan Apking <ryan.apking@gmail.com>
2016-08-26 10:06:45 -0700

    allow toppings to be removed by clicking on them in the Your Pizza section of the page

--- order-pizza/js/scripts.js
+++ order-pizza/js/scripts.js
@@ -29,6 +29,12 @@ Pizza.prototype.addTopping = function(topping) {
   this.setTotalPizzaPrice();
 };
 
+Pizza.prototype.removeTopping = function(topping) {
+  var index = this.toppings.indexOf(topping);
+  this.toppings.splice(index, 1);
+  this.setTotalPizzaPrice();
+};
+
 Pizza.prototype.changeSize = function(newSize) {
   this.size = newSize;
   this.setBasePrices();
@@ -50,45 +56,114 @@ $(document).ready(function() {
     $("#topping1").show();
     $("#topping1Btn").hide();
     currentPizza.addTopping("Green Pepper");
+    updatePriceDisplays();
   });
   $("#topping2Btn").click(function() {
     $("#topping2").show();
     $("#topping2Btn").hide();
     currentPizza.addTopping("Onion");
+    updatePriceDisplays();
   });
   $("#topping3Btn").click(function() {
     $("#topping3").show();
     $("#topping3Btn").hide();
     currentPizza.addTopping("Mushroom");
+    updatePriceDisplays();
   });
   $("#topping4Btn").click(function() {
     $("#topping4").show();
     $("#topping4Btn").hide();
     currentPizza.addTopping("Pineapple");
+    updatePriceDisplays();
   });
   $("#topping5Btn").click(function() {
     $("#topping5").show();
     $("#topping5Btn").hide();
     currentPizza.addTopping("Extra Cheese");
+    updatePriceDisplays();
   });
   $("#topping6Btn").click(function() {
     $("#topping6").show();
     $("#topping6Btn").hide();
     currentPizza.addTopping("Anchovies");
+    updatePriceDisplays();
   });
   $("#topping7Btn").click(function() {
     $("#topping7").show();
     $("#topping7Btn").hide();
     currentPizza.addTopping("Pepperoni");
+    updatePriceDisplays();
   });
   $("#topping8Btn").click(function() {
     $("#topping8").show();
     $("#topping8Btn").hide();
     currentPizza.addTopping("Bacon");
+    updatePriceDisplays();
   });
   $("#topping9Btn").click(function() {
     $("#topping9").show();
     $("#topping9Btn").hide();
     currentPizza.addTopping("Sausage");
+    updatePriceDisplays();
   });
+  $("#topping1").click(function() {
+    $("#topping1").hide();
+    $("#topping1Btn").show();
+    currentPizza.removeTopping("Green Pepper");
+    updatePriceDisplays();
+  });
+  $("#topping2").click(function() {
+    $("#topping2").hide();
+    $("#topping2Btn").show();
+    currentPizza.removeTopping("Onion");
+    updatePriceDisplays();
+  });
+  $("#topping3").click(function() {
+    $("#topping3").hide();
+    $("#topping3Btn").show();
+    currentPizza.removeTopping("Mushroom");
+    updatePriceDisplays();
+  });
+  $("#topping4").click(function() {
+    $("#topping4").hide();
+    $("#topping4Btn").show();
+    currentPizza.removeTopping("Pineapple");
+    updatePriceDisplays();
+  });
+  $("#topping5").click(function() {
+    $("#topping5").hide();
+    $("#topping5Btn").show();
+    currentPizza.removeTopping("Extra Cheese");
+    updatePriceDisplays();
+  });
+  $("#topping6").click(function() {
+    $("#topping6").hide();
+    $("#topping6Btn").show();
+    currentPizza.removeTopping("Anchovies");
+    updatePriceDisplays();
+  });
+  $("#topping7").click(function() {
+    $("#topping7").hide();
+    $("#topping7Btn").show();
+    currentPizza.removeTopping("Pepperoni");
+    updatePriceDisplays();
+  });
+  $("#topping8").click(function() {
+    $("#topping8").hide();
+    $("#topping8Btn").show();
+    currentPizza.removeTopping("Bacon");
+    updatePriceDisplays();
+  });
+  $("#topping9").click(function() {
+    $("#topping9").hide();
+    $("#topping9Btn").show();
+    currentPizza.removeTopping("Sausage");
+    updatePriceDisplays();
+  });
+
+  function updatePriceDisplays() {
+    $("#basePrice").text(currentPizza.basePrice.toFixed(2));
+    $(".toppingPrice").text(currentPizza.individualToppingPrice.toFixed(2));
+    $("#totalPrice").text(currentPizza.totalPizzaPrice.toFixed(2));
+  };
 });
