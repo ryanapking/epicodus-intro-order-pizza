fa113f633d4935f96f7f220e1c3d7957808053d7
Ryan Apking <ryan.apking@gmail.com>
2016-08-26 08:46:35 -0700

    add Pizza prototypes addTopping and changeSize

--- order-pizza/js/scripts.js
+++ order-pizza/js/scripts.js
@@ -4,31 +4,35 @@ function Pizza() {
   this.toppings = [];
   this.basePrice = 10;
   this.individualToppingPrice = 1;
-  this.totalToppingPrice = 0;
   this.totalPizzaPrice = 10;
 };
 
-Pizza.prototype.setBasePrices = function(size) {
-  if (size === "s") {
+Pizza.prototype.setBasePrices = function() {
+  if (this.size === "s") {
     this.basePrice = 10;
     this.individualToppingPrice = 1;
-  } else if (size === "m") {
+  } else if (this.size === "m") {
     this.basePrice = 12;
     this.individualToppingPrice = 1.5;
-  } else if (size === "l") {
+  } else if (this.size === "l") {
     this.basePrice = 14;
     this.individualToppingPrice = 2;
   }
 };
 
-Pizza.prototype.setIndividualToppingPrice = function() {
-
-};
-
-Pizza.prototype.setTotalToppingPrice = function() {
-
-};
-
 Pizza.prototype.setTotalPizzaPrice = function() {
-
+  this.totalPizzaPrice = this.basePrice + (this.toppings.length * this.individualToppingPrice);
 };
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+  this.setTotalPizzaPrice();
+};
+
+Pizza.prototype.changeSize = function(newSize) {
+  this.size = newSize;
+  this.setBasePrices();
+  this.setTotalPizzaPrice();
+};
+
+// Pizza.prototype.
